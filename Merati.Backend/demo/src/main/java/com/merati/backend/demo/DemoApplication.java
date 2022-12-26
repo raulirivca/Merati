a9b335427bfa0bc4e9b65afcfd44fb6d533ab4e0
Raul Carlos Abelardo Rivera Castañeda <raulriverac25@outlook.com>
2022-12-25 19:51:58 -0500

    Creacion de los Projectos

---
+++ Merati.Backend/demo/src/main/java/com/merati/backend/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.merati.backend.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
