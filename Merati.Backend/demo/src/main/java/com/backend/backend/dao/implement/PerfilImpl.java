868ef5af0374eb7e042e96de82f2c9d13ef62015
Raul Carlos Abelardo Rivera Castañeda <raulriverac25@outlook.com>
2022-12-26 16:08:06 -0500

    Mejora en el Dao

--- Merati.Backend/demo/src/main/java/com/backend/backend/dao/implement/PerfilImpl.java
+++ Merati.Backend/demo/src/main/java/com/backend/backend/dao/implement/PerfilImpl.java
@@ -20,5 +20,56 @@ public class PerfilImpl implements PerfilDao {
             throw new RuntimeException(throwables);
         }
     }
+
+    private void cerrarConexion(ResultSet resultado, Statement sentencia) {
+        try {
+            if (resultado != null) resultado.close();
+            if (sentencia != null) sentencia.close();
+            this.conexion.commit();
+            this.conexion.close();
+            this.conexion = null;
+        } catch (SQLException throwables) {
+            throw new RuntimeException(throwables);
+        }
+    }
     
+    /*
+    Para mostrar en la pantalla los roles a escoger en la creacion del usuario
+    */
+
+    @Override
+    public PerfilDto mostrarUsuarioResponsable() {
+        UsuarioDto usuario = new UsuarioDto();
+        String sql = "select u.dni_usuario as \"DNI RESPONSABLE\",\n" +
+                "u.nombres_usuario ||' '|| u.apellidos_usuario as \"NOMBRE RESPONSABLE\"\n" +
+                "from usuario u\n" +
+                "inner join orden o \n" +
+                "on o.id_usuario =u.id_usuario \n" +
+                "where o.id_orden =1";
+        try {
+            obtenerConexion();
+            Statement sentencia = conexion.createStatement();
+            ResultSet resultado = sentencia.executeQuery(sql);
+            while (resultado.next()){
+                usuario=(extraerTipoUsuario(resultado));
+            }
+            cerrarConexion(resultado,sentencia);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return usuario;
+    }
+
+    private PerfilDto extraerTipoUsuario(ResultSet resultado) throws SQLException {
+        PerfilDto retorno = new UsuarioDto(
+                null,
+                resultado.getString("DNI RESPONSABLE"),
+                resultado.getString("NOMBRE RESPONSABLE"),
+                null,
+                null,
+                null
+        );
+        return retorno;
+    }
+
 }
